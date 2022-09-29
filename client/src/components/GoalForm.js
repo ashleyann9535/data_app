8e3803ce8e8f5b214a58f0760e958780fcb007e4
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2022-09-28 22:37:53 -0500

    create dry goal form

---
+++ client/src/components/GoalForm.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const GoalForm = (props) => {
+  const { handleSubmit, buttonText, oldGoal } = props;
+  const [goal, setGoal] = useState(oldGoal || '');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    handleSubmit(goal);
+  };
+
+  return (
+    <div>
+      <h4> {buttonText} </h4>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          value={goal}
+          name="goal"
+          onChange={(e) => setGoal(e.target.value)}
+          className="m-1"
+          placeholder="Add goal..."
+        />
+        <input className="btn btn-secondary" type="submit" value={buttonText} />
+      </form>
+    </div>
+  );
+};
+
+export default GoalForm;
